5ddf31195a1f8edda423ea0bd20b8f17e5183f8d
DyTintegra <desarrollo.dtintegra@gmail.com>
2022-04-26 17:51:03 -0500

    archivos de servidor

---
+++ server_norun/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+//solve cors issue
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const port = 5000;
+const Pool = require('pg').Pool;
+  //Enter here your Postres database details
+  const pool = new Pool({
+    user: 'postgres',
+    host: '34.143.233.232',
+    database: 'garcal-db-test',
+    password: 'postgres',
+    dialect: 'postgres',
+    port: 5432
+});
+  
+  //Database connection and also please create postgres database first
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error(
+            'Error acquiring client', err.stack)
+    }
+    client.query('SELECT NOW()', (err, result) => {
+        release()
+        if (err) {
+            return console.error(
+                'Error executing query', err.stack)
+        }
+        console.log("Connected to Database !")
+    })
+})
+  
+app.get('/usuarios', (req, res, next) => {
+    console.log("Datos de usuarios :");
+    pool.query('Select * from public.usuarios_usu')
+        .then(testData => {
+            console.log(testData);
+            res.send(testData.rows);
+        })
+})
+app.listen(port, () => {
+  console.log(`Aplicacion corriendo en puerto ${port}.`);
+});
