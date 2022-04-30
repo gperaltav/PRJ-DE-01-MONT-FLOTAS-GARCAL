8ec856cb743fc93d6058966f26537477dc69b160
Cesar-x1 <darkheart.x1@outlook.com>
2022-04-30 12:52:39 -0500

    funciones de consulta completadas

--- src/database/functions.sql
+++ src/database/functions.sql
@@ -34,6 +34,62 @@ inner join
 using (emp_id)
 order by veh_feccreacion desc
 
+--
+create or replace function fun_get_vehiculos_all() 
+returns table(
+vid int,
+rs varchar,
+placa varchar,
+clase varchar,
+tipo varchar,
+marca varchar,
+modelo varchar,
+anno int,
+serie varchar,
+mtc varchar,
+cargautil numeric,
+km numeric
+)
+LANGUAGE 'plpgsql'
+as
+$$
+declare 
+begin
+
+RETURN QUERY SELECT
+veh_id as vid,
+emp_razonsocial as rs,
+veh_placa as placa,
+vcl_nombre as clase,
+vti_nombre as tipo,
+vma_nombre as marca,
+vmo_nombre as modelo,
+veh_anno as anno,
+veh_serie as serie,
+veh_mtc as mtc,
+veh_cargautil as cargautil,
+veh_kilometraje as km
+FROM
+((((operaciones.vehiculos_veh
+inner join
+operaciones.vehiculosmarcas_vma 
+USING (vma_id))
+inner join
+operaciones.vehiculostipos_vti
+USING (vti_id))
+inner join
+operaciones.vehiculosclases_vcl
+USING (vcl_id))
+inner join
+operaciones.vehiculosmodelos_vmo
+USING (vmo_id))
+inner join
+(select emp_id,emp_razonsocial from public.empresas_emp ) as empresas
+using (emp_id)
+order by veh_feccreacion desc ;
+end;
+$$
+
 -- Seleccionar clientes
 SELECT
 *,concat_ws(' ',ent_nombre,ent_apellidopaterno, ent_apellidomaterno) as nnombre
@@ -49,6 +105,42 @@ inner join
 (select emp_id,emp_razonsocial from public.empresas_emp ) as empresas
 using (emp_id)
 order by ent_feccreacion desc
+
+--
+
+create or replace function fun_get_clientes_all() 
+returns table(
+	rs varchar,
+	nnombre text,
+	pnatural bool,
+	ndoc varchar
+)
+LANGUAGE 'plpgsql'
+as
+$$
+declare 
+begin
+
+RETURN QUERY 
+SELECT
+emp_razonsocial,
+concat_ws(' ',ent_nombre,ent_apellidopaterno, ent_apellidomaterno) as nnombre,
+ent_personanatural,
+ent_nrodocumento
+FROM
+(((select * from public.entidadestiposxentidades_exe WHERE ext_id='cli') as exe
+inner join
+public.entidades_ent
+using (ent_id))
+inner join
+public.entidadestipos_ext
+using (ext_id))
+inner join 
+(select emp_id,emp_razonsocial from public.empresas_emp ) as empresas
+using (emp_id)
+order by ent_feccreacion desc;
+end;
+$$
 
 -- Seleccionar proveedores
 SELECT
@@ -66,8 +158,63 @@ inner join
 using (emp_id)
 order by ent_feccreacion desc
 
+--
 
 
+create or replace function fun_get_proveedores_all() 
+returns table(
+	rs varchar,
+	nnombre text,
+	pnatural bool,
+	ndoc varchar
+)
+LANGUAGE 'plpgsql'
+as
+$$
+declare 
+begin
+
+RETURN QUERY 
+SELECT
+emp_razonsocial,
+concat_ws(' ',ent_nombre,ent_apellidopaterno, ent_apellidomaterno) as nnombre,
+ent_personanatural,
+ent_nrodocumento
+FROM
+(((select * from public.entidadestiposxentidades_exe WHERE ext_id='prv') as exe
+inner join
+public.entidades_ent
+using (ent_id))
+inner join
+public.entidadestipos_ext
+using (ext_id))
+inner join 
+(select emp_id,emp_razonsocial from public.empresas_emp ) as empresas
+using (emp_id)
+order by ent_feccreacion desc
+end;
+$$
+
+-- Seleccionar trabajadores
+
+SELECT
+tra_id,
+emp_razonsocial,
+concat_ws(' ',tra_nombres,tra_apellidopaterno,tra_apellidomaterno) as tnommbre,
+tra_nrodocumento,
+pue_nombre,
+tra_tipocontrati,
+tra_fechaingreso,
+tra_fechacese
+FROM
+(rrhh.trabajadores_tra
+inner join
+rrhh.puestos_pue
+using (pue_id))
+inner join
+(select emp_id,emp_razonsocial from public.empresas_emp ) as empresas
+using (emp_id)
+order by tra_feccreacion desc;
 
 --||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||--
 
